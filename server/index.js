f35b79beaa258cc6fc23ed1ef1dbff3d35203bd5
Nicholas Nadar <nnadar12@gmail.com>
2025-09-18 14:14:34 -0400

    added upload route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
-const http = require('http');
+/*const http = require('http');
 
 const server = http.createServer((req, res) => {
-	  res.writeHead(200, { 'Content-Type': 'text/plain' });
-	  res.end('Hello, Node.js Web Server!');
+//	  res.writeHead(200, { 'Content-Type': 'text/plain' });
+//	  res.end('Hello, Node.js Web Server!');
+	
 });
 
 const port = 3000;
 server.listen(port, () => {
 	  console.log(`Server running at http://localhost:${port}/`);
+});*/
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send("Hello World");
+});
+
+app.post('/api/upload', (req, res) => {
+	res.send("uploaded succesfully");
+	res.send("uploaded successfully");
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log("listening on port" + port);
 });
